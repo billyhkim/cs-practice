a6f36fa804f5f288b2c4397d3a69b3db27c5c8ae
Billy Kim <billyhkim.dev@gmail.com>
2019-12-22 08:46:55 -0800

    lc-3 brute force method

--- lc/3.js
+++ lc/3.js
@@ -2,29 +2,56 @@
 Given a string, find the length of the longest substring without repeating characters.
 */
 var lengthOfLongestSubstring = function(s) {
-  var stringArray = s.split('');
-  var longestSubstring = '';
-  var newString = '';
+  // var stringArray = s.split('');
+  // var longestSubstring = '';
+  // var newString = '';
   
-  // log every substring before a repeating character
-  for (var i = 0; i < stringArray.length; i++) {
-    if (!newString.includes(stringArray[i])) {
-      newString += stringArray[i]; 
-    } else {
-      if (newString.length > longestSubstring.length) {
-        longestSubstring = newString;
+  // // log every substring before a repeating character
+  // for (var i = 0; i < stringArray.length; i++) {
+  //   if (!newString.includes(stringArray[i])) {
+  //     newString += stringArray[i]; 
+  //   } else {
+  //     if (newString.length > longestSubstring.length) {
+  //       longestSubstring = newString;
+  //     }
+  //     newString = '';
+  //     newString += stringArray[i];
+  //   }
+  // }
 
+  // if (newString.length > longestSubstring.length) {
+  //   longestSubstring = newString;
+  // }
+
+  // return longestSubstring.length;
+
+  var longestSubstringLength = 0;
+
+  for (var i = 0; i < s.length; i++) {
+    for (var j = i + 1; j <= s.length; j++) {
+      if (duplicateCheck(s, i, j)) {
+        longestSubstringLength = Math.max(longestSubstringLength, j - i);
       }
-      newString = '';
-      newString += stringArray[i];
     }
   }
 
-  if (newString.length > longestSubstring.length) {
-    longestSubstring = newString;
-  }
-
-  return longestSubstring.length;
+  return longestSubstringLength;
 };
 
-lengthOfLongestSubstring("abcabcbb");
+var duplicateCheck = function(string, start, end) {
+  var stringSet = new Set();
+
+  for (var i = start; i < end; i++) {
+    var stringValue = string.charAt(i);
+
+    if (stringSet.has(stringValue)) {
+      return false;
+    }
+
+    stringSet.add(stringValue);
+  }
+
+  return true;
+}
+
+lengthOfLongestSubstring("dvdf");
