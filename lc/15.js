360fab462f326176dc51bca6ba06b2a73ad590a8
Billy Kim <billyhkim.dev@gmail.com>
2020-01-25 10:40:17 -0800

    added 15.js with duplicate results

---
+++ lc/15.js
@@ -0,0 +1,25 @@
+/*
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Note: The solution set must not contain duplicate triplets.
+*/
+
+const threeSum = (numsArr) => {
+  let resultArr = [];
+  let storeArr = [];
+
+  for (let i = 0; i < numsArr.length; i+=1) {
+    for (let j = 1; j < numsArr.length; j+=1) {
+      for (let k = 2; k < numsArr.length; k+=1) {
+        if (numsArr[i] + numsArr[j] + numsArr[k] === 0) {
+          resultArr.push([numsArr[i], numsArr[j], numsArr[k]]);
+        }
+      }
+    }
+  }
+
+  console.log(resultArr);
+  return resultArr;
+};
+
+threeSum([-1, 0, 1, 2, -1, -4]);
