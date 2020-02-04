0f05744f1f91cbf33663884947a6242e89d49d8f
Billy Kim <billyhkim.dev@gmail.com>
2020-02-03 21:37:06 -0800

    lc 17.js using dfs

--- lc/17.js
+++ lc/17.js
@@ -8,3 +8,36 @@ Input: "23"
 Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 */
 
+const phoneValues = {
+  2: ['a', 'b', 'c'],
+  3: ['d', 'e', 'f'],
+  4: ['g', 'h', 'i'],
+  5: ['j', 'k', 'l'],
+  6: ['m', 'n', 'o'],
+  7: ['p', 'q', 'r', 's'],
+  8: ['t', 'u', 'v'],
+  9: ['w', 'x', 'y', 'z'],
+}
+
+const letterCombinations = function(digits) {
+  if (digits.length === 0) return [];
+  
+  let result = [];
+
+  const depthFirstSearch = (index, word) => {
+    if (index < digits.length) {
+      let letter = digits[index];
+      phoneValues[letter].forEach((letter) => {
+        depthFirstSearch(index + 1, word + letter);
+      })
+    } else {
+      result.push(word);
+    }
+  }
+
+  depthFirstSearch(0, '');
+  console.log(result);
+  return result;
+};
+
+letterCombinations('23');
