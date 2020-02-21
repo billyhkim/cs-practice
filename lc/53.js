00cd4d8fc2f117d211a38e336e3530224635b75e
Billy Kim <billyhkim.dev@gmail.com>
2020-02-20 19:17:44 -0800

    completed lc 53 o^n2

---
+++ lc/53.js
@@ -0,0 +1,33 @@
+/*
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Follow up:
+
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+*/
+
+const maxSubArray = function(nums) {
+  if (nums.length === 1) return nums[0]; 
+  
+  let result = nums[0];
+  let sum = 0;
+  
+  for (let i = 0; i < nums.length; i++) {
+    sum = nums[i];
+    if (sum > result) result = sum;
+    
+    for (let j = i + 1; j < nums.length; j++) {
+      sum += nums[j];
+      if (sum > result) result = sum;
+    }
+  }
+  
+  return result;
+};
+
+maxSubArray([-2,1,-3,4,-1,2,1,-5,4]);
