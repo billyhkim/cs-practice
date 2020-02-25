8ff1aeb9bf8dcff9764a99ac04815f2732c4bfd3
Billy Kim <billyhkim.dev@gmail.com>
2020-02-25 14:01:39 -0800

    updated lc 152 indentation

--- lc/152.js
+++ lc/152.js
@@ -18,22 +18,21 @@ const maxProduct = nums => {
   let result = nums[0];
 
   for (let i = 1; i < nums.length; i++) {
-      // given the new number, the new maximun can have 3 conditions
-      // 1. number(+) * prevMax(+) is the largest
-      // 2. number(+) it self is the largest
-      // 3. number(-) * prevMin(-) is the largest 
-      curMax = Math.max(nums[i] * prevMax, nums[i], nums[i] * prevMin);
-      
-      curMin = Math.min(nums[i] * prevMin, nums[i], nums[i] * prevMax);
+    // given the new number, the new maximun can have 3 conditions
+    // 1. number(+) * prevMax(+) is the largest
+    // 2. number(+) it self is the largest
+    // 3. number(-) * prevMin(-) is the largest 
+    curMax = Math.max(nums[i] * prevMax, nums[i], nums[i] * prevMin);
+    curMin = Math.min(nums[i] * prevMin, nums[i], nums[i] * prevMax);
 
-  // updating the prevMax & prevMin, these two may swap locations
-      prevMax = curMax;
-      prevMin = curMin;
+    // updating the prevMax & prevMin, these two may swap locations
+    prevMax = curMax;
+    prevMin = curMin;
 
-      result = Math.max(curMax, result);
+    result = Math.max(curMax, result);
   }
   return result;
 };
 
 // maxProduct([2,3,-2,4]);
-maxProduct([-2,3,-4]);
+maxProduct([-2,3,-4,10,-8,5]);
