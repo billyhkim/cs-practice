e2ee8064733c98e33866d0df3a80b707f4fb9648
Billy Kim <billyhkim.dev@gmail.com>
2020-02-25 11:10:07 -0800

    completed lc 152 with comments

---
+++ lc/152.js
@@ -0,0 +1,39 @@
+/*
+Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+Example 1:
+Input: [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+
+Example 2:
+Input: [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+*/
+
+const maxProduct = nums => {
+  let prevMax = nums[0];
+  let prevMin = nums[0];
+  let result = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+      // given the new number, the new maximun can have 3 conditions
+      // 1. number(+) * prevMax(+) is the largest
+      // 2. number(+) it self is the largest
+      // 3. number(-) * prevMin(-) is the largest 
+      curMax = Math.max(nums[i] * prevMax, nums[i], nums[i] * prevMin);
+      
+      curMin = Math.min(nums[i] * prevMin, nums[i], nums[i] * prevMax);
+
+  // updating the prevMax & prevMin, these two may swap locations
+      prevMax = curMax;
+      prevMin = curMin;
+
+      result = Math.max(curMax, result);
+  }
+  return result;
+};
+
+// maxProduct([2,3,-2,4]);
+maxProduct([-2,3,-4]);
