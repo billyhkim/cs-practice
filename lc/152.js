97545769e5a87bdfa7687e2e9d64093a9ccae8cb
Billy Kim <billyhkim.dev@gmail.com>
2020-02-25 14:11:52 -0800

    completed lc 153

--- lc/152.js
+++ lc/152.js
@@ -20,7 +20,7 @@ const maxProduct = nums => {
   for (let i = 1; i < nums.length; i++) {
     // given the new number, the new maximun can have 3 conditions
     // 1. number(+) * prevMax(+) is the largest
-    // 2. number(+) it self is the largest
+    // 2. number(+) itself is the largest
     // 3. number(-) * prevMin(-) is the largest 
     curMax = Math.max(nums[i] * prevMax, nums[i], nums[i] * prevMin);
     curMin = Math.min(nums[i] * prevMin, nums[i], nums[i] * prevMax);
