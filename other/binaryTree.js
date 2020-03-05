6a6d4dbd21718d66d09d573ee7d3ce147797c24d
Billy Kim <billyhkim.dev@gmail.com>
2020-03-04 23:40:28 -0800

    completed binaryTree with insert and traversal

---
+++ other/binaryTree.js
@@ -0,0 +1,92 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(val) {
+    let newNode = new Node(val);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.val < node.val) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  inOrderTraversal(node) {
+    if (node !== null) {
+      this.inOrderTraversal(node.left);
+      console.log(node.val);
+      this.inOrderTraversal(node.right);
+    }
+  }
+
+  preOrderTraversal(node) {
+    if (node !== null) {
+      console.log(node.val);
+      this.preOrderTraversal(node.left);
+      this.preOrderTraversal(node.right);
+    }
+  }
+
+  postOrderTraversal(node) {
+    if (node !== null) {
+      this.postOrderTraversal(node.left);
+      this.postOrderTraversal(node.right);
+      console.log(node.val);
+    }
+  }
+
+  getRootNode() {
+    return this.root;
+  }
+}
+
+let BST = new BinarySearchTree();
+BST.insert(15);
+BST.insert(25);
+BST.insert(10);
+BST.insert(7);
+BST.insert(22);
+BST.insert(17);
+BST.insert(13);
+BST.insert(5);
+BST.insert(9);
+BST.insert(27);
+
+let root = BST.getRootNode();
+// BST.inOrderTraversal(root);
+// BST.preOrderTraversal(root);
+BST.postOrderTraversal(root);
+
+//          15
+//         /  \
+//        10   25
+//       / \   / \
+//      7  13 22  27
+//     / \    /
+//    5   9  17
+
